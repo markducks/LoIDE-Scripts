7635bbdbadc4ef36eb3550777dec4b9c0689bd57
Marco Duca <dcumrc02a07d086r@studenti.unical.it>
2024-09-22 18:45:33 +0200

    Added base paths and updated the deploy scripts

--- DeployESE.sh
+++ DeployESE.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-CONFIG_PATH_ESE=""
-LOGS_PATH_ESE=""
+BASE_PATH=""
+CONFIG_PATH_ESE="LoIDE_data/ese/config"
+LOGS_PATH_ESE="LoIDE_data/ese/logs"
 
 # Pull and run the latest version of the ESE
 # ADD THE PATH TO THE CONFIGURATION FOLDER IN THE --mount OPTION
 sudo docker pull loideunical/loide:ese
-nohup docker run --network host --mount type=bind,source=CONFIG_PATH_ESE,target=/config_files --privileged=true --restart=always --name ese loideunical/loide:ese >> LOGS_PATH_ESE 2>&1 &
+nohup docker run --network host --mount type=bind,source=$BASE_PATH/$CONFIG_PATH_ESE,target=/config_files --privileged=true --restart=always --name ese loideunical/loide:ese >> $BASE_PATH/$LOGS_PATH_ESE 2>&1 &
--- DeployPWA-API.sh
+++ DeployPWA-API.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-CONFIG_PATH_API=""
-LOGS_PATH_API=""
-CONFIG_PATH_PWA=""
-LOGS_PATH_PWA=""
+# CONFIG_PATH_API and CONFIG_PATH_PWA are the paths to the configuration folders of the API and PWA, respectively.
+# LOGS_PATH_API and LOGS_PATH_PWA are the paths to the log files of the API and PWA, respectively.
+# API_SERVER is the IP address of the API server.
+BASE_PATH=""
+CONFIG_PATH_API="LoIDE_data/api/config"
+LOGS_PATH_API="LoIDE_data/api/logs"
+CONFIG_PATH_PWA="LoIDE_data/pwa/config"
+LOGS_PATH_PWA="LoIDE_data/pwa/logs"
 API_SERVER=""
 
 # Pull and run the latest version of the API
 # ADD THE PATH TO THE CONFIGURATION FOLDER IN THE --mount OPTION
 docker pull loideunical/loide:api
-nohup docker run --network host --mount type=bind,source=CONFIG_PATH,target=/app/config --restart=always --name api loideunical/loide:api >> LOGS_PATH_PWA 2>&1 &
+nohup docker run --network host --mount type=bind,source=$BASE_PATH/$CONFIG_PATH_API,target=/app/config --restart=always --name api loideunical/loide:api >> $BASE_PATH/$LOGS_PATH_API 2>&1 &
 
 # Pull and run the latest version of the PWA
 # ADD THE PATH TO THE CONFIGURATION FOLDER IN THE --mount OPTION
 docker pull loideunical/loide:pwa
-nohup docker run --network host --env REACT_APP_LOIDE_API_SERVER=API_SERVER --mount type=bind,source=CONFIG_PATH,target=/app/config --restart=always --name pwa loideunical/loide:pwa >> LOGS_PATH_API 2>&1 &
+nohup docker run --network host --env REACT_APP_LOIDE_API_SERVER=API_SERVER --mount type=bind,source=$BASE_PATH/$CONFIG_PATH_PWA,target=/app/config --restart=always --name pwa loideunical/loide:pwa >> $BASE_PATH/$LOGS_PATH_PWA 2>&1 &
 
