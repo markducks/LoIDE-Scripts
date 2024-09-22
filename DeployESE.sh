8cb896d56a950e12e9351c50db5f408f471e70da
Marco Duca <dcumrc02a07d086r@studenti.unical.it>
2024-09-22 18:18:27 +0200

    Changes

--- DeployESE.sh
+++ DeployESE.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+CONFIG_PATH_ESE=""
+LOGS_PATH_ESE=""
+
 # Pull and run the latest version of the ESE
 # ADD THE PATH TO THE CONFIGURATION FOLDER IN THE --mount OPTION
 sudo docker pull loideunical/loide:ese
-sudo docker run -d --network host --mount type=bind,source=,target=/app/config --privileged=true --restart=always loideunical/loide:ese
-
-# Save the logs of ESE
-sudo docker logs -f -t loideunical/loide:ese >& loide/ese/logs/ese-logs.log
+nohup docker run --network host --mount type=bind,source=CONFIG_PATH_ESE,target=/config_files --privileged=true --restart=always --name ese loideunical/loide:ese >> LOGS_PATH_ESE 2>&1 &
--- DeployPWA-API.sh
+++ DeployPWA-API.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+CONFIG_PATH_PWA=""
+CONFIG_PATH_API=""
+LOGS_PATH_PWA=""
+LOGS_PATH_API=""
+API_SERVER=""
+
 # Pull and run the latest version of the API
 # ADD THE PATH TO THE CONFIGURATION FOLDER IN THE --mount OPTION
-nohup docker run --network host --mount type=bind,source=/home/marco/Desktop/API-config/config,target=/app/config --name api loideunical/loide:api >> loide/api/logs/api-logs.log 2>&1 &
+docker pull loideunical/loide:pwa
+nohup docker run --network host --mount type=bind,source=CONFIG_PATH,target=/app/config --restart=always --name api loideunical/loide:api >> LOGS_PATH_PWA 2>&1 &
 
 # Pull and run the latest version of the PWA
 # ADD THE PATH TO THE CONFIGURATION FOLDER IN THE --mount OPTION
-nohup docker run --network host --env REACT_APP_LOIDE_API_SERVER="" --mount type=bind,source=/home/marco/Desktop/PWA-config/config,target=/app/config --name pwa loideunical/loide:pwa >> loide/pwa/logs/pwa-logs.log 2>&1 &
+docker pull loideunical/loide:pwa
+nohup docker run --network host --env REACT_APP_LOIDE_API_SERVER=API_SERVER --mount type=bind,source=CONFIG_PATH,target=/app/config --restart=always --name pwa loideunical/loide:pwa >> LOGS_PATH_API 2>&1 &
+
