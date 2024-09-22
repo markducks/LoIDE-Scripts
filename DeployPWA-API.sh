c57eb4aa7eb390a0287a28c30ef269354bd7b23f
Marco Duca <dcumrc02a07d086r@studenti.unical.it>
2024-09-22 14:50:41 +0200

    Added env variable for PWA

--- DeployPWA-API.sh
+++ DeployPWA-API.sh
@@ -3,7 +3,7 @@
 # Pull and run the latest version of the PWA
 # ADD THE PATH TO THE CONFIGURATION FOLDER IN THE --mount OPTION
 sudo docker pull loideunical/loide:pwa
-sudo docker run -d --network host --mount type=bind,source=,target=/app/config --restart=always loideunical/loide:pwa
+sudo docker run -d --network host --env REACT_APP_LOIDE_API_SERVER="" --mount type=bind,source=,target=/app/config --restart=always loideunical/loide:pwa
 
 # Save the logs of PWA
 sudo docker logs -f -t loideunical/loide:pwa >& loide/pwa/logs/pwa-logs.log
