799d0bb7efd6b441a8825add19f36e2df8438846
Marco Duca <dcumrc02a07d086r@studenti.unical.it>
2024-09-22 19:53:32 +0200

    Eliminated a couple of comments

--- DeployESE.sh
+++ DeployESE.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+#CONFIG_PATH_ESE is the path to the configuration folder of the ESE.
+#LOGS_PATH_ESE is the path to the log file of the ESE.
 BASE_PATH=""
 CONFIG_PATH_ESE="$BASE_PATH/ese/config"
 LOGS_PATH_ESE="$BASE_PATH/ese/logs/ese.log"
 
 # Pull and run the latest version of the ESE
-# ADD THE PATH TO THE CONFIGURATION FOLDER IN THE --mount OPTION
 sudo docker pull loideunical/loide:ese
 nohup docker run --network host --mount type=bind,source=$CONFIG_PATH_ESE,target=/config_files --privileged=true --restart=always --name ese loideunical/loide:ese >> $LOGS_PATH_ESE 2>&1 &
--- DeployPWA-API.sh
+++ DeployPWA-API.sh
@@ -11,11 +11,9 @@ LOGS_PATH_PWA="$BASE_PATH/pwa/logs/pwa.log"
 API_SERVER=""
 
 # Pull and run the latest version of the API
-# ADD THE PATH TO THE CONFIGURATION FOLDER IN THE --mount OPTION
 docker pull loideunical/loide:api
 nohup docker run --network host --mount type=bind,source=$CONFIG_PATH_API,target=/app/config --restart=always --name api loideunical/loide:api >> $LOGS_PATH_API 2>&1 &
 
 # Pull and run the latest version of the PWA
-# ADD THE PATH TO THE CONFIGURATION FOLDER IN THE --mount OPTION
 docker pull loideunical/loide:pwa
 nohup docker run --network host --env REACT_APP_LOIDE_API_SERVER=$API_SERVER --mount type=bind,source=$CONFIG_PATH_PWA,target=/app/config --restart=always --name pwa loideunical/loide:pwa >> $LOGS_PATH_PWA 2>&1 &
