e7fddf8de729a37f75347d0ce1b6608df83462cd
Marco Duca <dcumrc02a07d086r@studenti.unical.it>
2024-09-22 18:05:08 +0200

    Changed the DeployPWA-API.sh file

--- DeployPWA-API.sh
+++ DeployPWA-API.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-# Pull and run the latest version of the PWA
-# ADD THE PATH TO THE CONFIGURATION FOLDER IN THE --mount OPTION
-sudo docker pull loideunical/loide:pwa
-sudo docker run -d --network host --env REACT_APP_LOIDE_API_SERVER="" --mount type=bind,source=,target=/app/config --restart=always loideunical/loide:pwa
-
-# Save the logs of PWA
-sudo docker logs -f -t loideunical/loide:pwa >& loide/pwa/logs/pwa-logs.log
-
 # Pull and run the latest version of the API
 # ADD THE PATH TO THE CONFIGURATION FOLDER IN THE --mount OPTION
-sudo docker pull loideunical/loide:api
-sudo docker run -d --network host --mount type=bind,source=,target=/app/config --restart=always loideunical/loide:api
+nohup docker run --network host --mount type=bind,source=/home/marco/Desktop/API-config/config,target=/app/config --name api loideunical/loide:api >> loide/api/logs/api-logs.log 2>&1 &
 
-# Save the logs of API
-sudo docker logs -f -t loideunical/loide:api >& loide/api/logs/api-logs.log
+# Pull and run the latest version of the PWA
+# ADD THE PATH TO THE CONFIGURATION FOLDER IN THE --mount OPTION
+nohup docker run --network host --env REACT_APP_LOIDE_API_SERVER="" --mount type=bind,source=/home/marco/Desktop/PWA-config/config,target=/app/config --name pwa loideunical/loide:pwa >> loide/pwa/logs/pwa-logs.log 2>&1 &
